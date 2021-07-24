9f6960073e7f1cfb835f6d923742926f2895fc35
liuquan <lauchuen94@gmail.com>
2021-07-24 11:59:06 +0800

    add local source parser, exm test

---
+++ shark_example/lib/widget_json.dart
@@ -0,0 +1,18 @@
+
+const container = '''
+{
+  "type": "Container",
+  "color": "#FF00FF",
+  "alignment": "center",
+  "child": {
+    "type": "Text",
+    "data": "Flutter dynamic widget",
+    "maxLines": 3,
+    "overflow": "ellipsis",
+    "style": {
+      "color": "#00FFFF",
+      "fontSize": 20.0
+    }
+  }
+}
+''';
